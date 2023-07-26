badc6a49def988511012ad9e452c198d1648ab7b
Ngo Brain <dalamity@dalamabonk.localdomain>
2023-07-26 13:39:52 -0700

    Finished Parser Printing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod scanner;
+mod parser;
 use std::io::BufRead;
 
+use parser::Parser;
 
 fn main() {
     let mut args = std::env::args();
@@ -13,16 +15,50 @@ fn main() {
     let infile_lines = std::io::BufReader::new(in_file).lines();
     
     let mut output = String::new();
+    
+
+    
     for line in infile_lines {
         let line = line.unwrap();
-        output.push_str(&format!("Line: {}", line));
-        let tokens = scanner::parse_token(line);
-        for mut token in tokens {
-            if !scanner::match_num_or_identifier(&mut token, &mut output) {
+        if line.replace(" ", "").is_empty() {
+            continue;
+        }
+        
+        // Scanner
+        let mut scanner_results = String::new();
+        scanner_results.push_str(&format!("Line: {}", line));
+        let mut valid = true;
+        let mut tmp_tokens = Vec::new();
+        let mut input_tokens = scanner::parse_token(line);
+        for mut token in input_tokens.clone() {
+            let scan = scanner::match_num_or_identifier(&mut token, &mut scanner_results);
+            if !scan.0 {
+                valid = false;
                 break;
             }
+            
+            for token in scan.1 {
+                tmp_tokens.push(token);
+            }
+            
         }
-        output = format!("{}\n\n", output);
+        
+        if !valid {
+            output = format!("{}{}\n", output, scanner_results);
+            break;
+        }
+        
+        // Regenerate Tokens Properly
+        input_tokens = tmp_tokens;
+        
+        // Parser
+        let mut parser_results = String::new();
+        let tree = parser::parse_tokens(&mut input_tokens);
+        parser::printTree(&tree, &mut parser_results, &mut 0);
+        
+        
+        // Write Scanner Results to output
+        output = format!("{}{}\n\n{}\n\n", output, scanner_results, parser_results);
     }
     
     write_output(output, args.nth(0).expect("No output file detected"));
